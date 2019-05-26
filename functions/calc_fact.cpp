fc7240df6e79dcc1b557c283fca8eec31a7e7ad5
Hiroki Shibata <s17097@tokyo.kosen-ac.jp>
2019-05-26 12:30:16 +0900

    [change]change to use modint in modpow and calc_fact

--- functions/calc_fact.cpp
+++ functions/calc_fact.cpp
@@ -1,14 +1,13 @@
 auto calc_fact = [mpow]{
 	constexpr int N = 1e7;
-	constexpr int MOD = 1e9 + 7;
-	vector<i64> fact(N + 1, 1);
-	vector<i64> inv(N + 1, 1);
+	vector<mint> fact(N + 1, 1);
+	vector<mint> inv(N + 1, 1);
 	for(int i = 1; i < N; ++i){
-		fact[i + 1] = fact[i] * (i + 1) % MOD;
+		fact[i + 1] = fact[i] * (i + 1);
 		inv[i + 1] = mpow(fact[i + 1], MOD - 2);
 	}
 	return make_pair(fact, inv);
 };
-vector<i64> fact, inv;
+vector<mint> fact, inv;
 tie(fact, inv) = calc_fact();
 
--- functions/modpow.cpp
+++ functions/modpow.cpp
@@ -1,9 +1,9 @@
-auto mpow = [&](i64 x, i64 y){
-	i64 z = x;
-	i64 val = y & 1 ? x : 1L;
-	while(z = z * z % MOD, y >>= 1)
+auto mpow = [&](mint x, i64 y){
+	mint z = x;
+	mint val = y & 1 ? x : mint(1);
+	while(z *= z, y >>= 1)
 		if(y & 1)
-			val = val * z % MOD;
+			val *= z;
 	return val;
 };
 

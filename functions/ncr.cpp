228395d2e3493336c42ef48ec4ac54e5b799955b
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-05-29 08:39:39 +0900

    [update]ncr out_of_range check

--- functions/ncr.cpp
+++ functions/ncr.cpp
@@ -1,4 +1,6 @@
 auto ncr = [&fact, &inv](int n, int r){
-	return fact[n] * inv[r] % MOD * inv[n - r] % MOD;
+	if(n < 0 || r < 0 || n < r)
+		return mint(0);
+	return fact[n] * inv[r] inv[n - r];
 };
 

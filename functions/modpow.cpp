a769732053f96ecc544fa8c0677ff0251682a6a0
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-07-11 06:35:43 +0900

    [update]modpow function template

--- functions/modpow.cpp
+++ functions/modpow.cpp
@@ -1,6 +1,7 @@
-auto mpow = [&](mint x, i64 y){
-	mint z = x;
-	mint val = y & 1 ? x : mint(1);
+template <typename T = mint>
+auto mpow = [](T x, i64 y){
+	T z = x;
+	T val = y & 1 ? x : T(1);
 	while(z *= z, y >>= 1)
 		if(y & 1)
 			val *= z;

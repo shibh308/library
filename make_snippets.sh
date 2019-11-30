cfb2fdb197f6006367651e9efea0c0a8b68dd93f
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-11-30 19:14:10 +0900

    [add]compro_library and make snippets

---
+++ make_snippets.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+git config --global user.name "shibh308"
+git config --global user.email "s17097@tokyo.kosen-ac.jp"
+
+git remote set-url origin https://shibh308:${GITHUB_TOKEN}@github.com/shibh308/library.git
+
+git checkout -b master
+git branch -a
+python3 make_snippets.py
+git add compro_library.xml
+git commit -m '[auto]update snippets'
+git push origin HEAD

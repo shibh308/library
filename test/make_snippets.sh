1310f8323fa1a21e9c9d9d37c8b4177e459cf9ad
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-11-30 22:54:01 +0900

    [fix]make_snippets.sh

--- test/make_snippets.sh
+++ test/make_snippets.sh
@@ -7,10 +7,11 @@ git remote set-url origin https://shibh308:${GITHUB_TOKEN}@github.com/shibh308/l
 git checkout -b master
 git branch -a
 
+cp compro_library.xml.tmp
 python3 test/make_snippets.py
 
-if ! diff $1 $2 -q >/dev/null ; then
-	git add compro_library.xml
+if ! diff test/compro_library.xml test/compro_library.xml.tmp  -q ; then
+	git add test/compro_library.xml
 	git commit -m '[auto]update snippets'
 	git push origin HEAD
 fi

160f4cbde8bd4ef71f9f74b56b004aa53c4645d3
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-11-30 19:32:28 +0900

    [update]make_snippets.sh

--- make_snippets.sh
+++ make_snippets.sh
@@ -6,7 +6,11 @@ git remote set-url origin https://shibh308:${GITHUB_TOKEN}@github.com/shibh308/l
 
 git checkout -b master
 git branch -a
+
 python3 make_snippets.py
-git add compro_library.xml
-git commit -m '[auto]update snippets'
-git push origin HEAD
+
+if ! diff $1 $2 -q >/dev/null ; then
+	git add compro_library.xml
+	git commit -m '[auto]update snippets'
+	git push origin HEAD
+fi

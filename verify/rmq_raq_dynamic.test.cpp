90bb0f193dc34c705cfc48a1b8f53e74b390f360
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-12-05 02:54:01 +0900

    [add]verify

---
+++ verify/rmq_raq_dynamic.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H"
+
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long;
+
+#include "../lib/classes/dynamiclazysegmenttree.cpp"
+
+
+signed main() {
+    int n, q;
+    cin >> n >> q;
+    Segtree<int,int> seg(n, [](auto x, auto y){return min(x, y);}, [](auto x, auto y, int){return x + y;}, [](auto x, auto y){return x + y;}, 1e9, 0);
+    seg.update(0, n, -1e9);
+    for(int i = 0; i < q; ++i){
+        int ty;
+        cin >> ty;
+        if(ty == 0){
+            int s, t, x;
+            cin >> s >> t >> x;
+            seg.update(s, ++t, x);
+        }
+        else{
+            int s, t;
+            cin >> s >> t;
+            cout << seg.get(s, ++t) << endl;
+        };
+    }
+}

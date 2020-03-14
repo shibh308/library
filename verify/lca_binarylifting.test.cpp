4431d4b94b580aa723b48f1a038882c0d72a838a
shibh308 <s17097@tokyo.kosen-ac.jp>
2020-03-14 13:35:11 +0900

    [add]binarylifting verify

---
+++ verify/lca_binarylifting.test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+
+#include "../lib/classes/binarylifting.cpp"
+
+signed main() {
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> edges(n);
+    for(int i = 1; i < n; ++i){
+        int j;
+        cin >> j;
+        edges[i].emplace_back(j);
+        edges[j].emplace_back(i);
+    }
+    BinaryLifting tree(edges);
+    for(int i = 0; i < q; ++i){
+        int u, v;
+        cin >> u >> v;
+        cout << tree.lca(u, v) << endl;
+    }
+}
+

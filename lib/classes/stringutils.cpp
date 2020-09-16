ce12edff7dcc62f61d2511738553f9168730a4e1
shibh308 <s17097@tokyo.kosen-ac.jp>
2020-09-16 20:22:56 +0900

    [add]sa verify

--- lib/classes/stringutils.cpp
+++ lib/classes/stringutils.cpp
@@ -1,14 +1,14 @@
 struct StringUtils{
     int n;
     string s;
-    vector<int> sa, sa_inv, lcp;
-    StringUtils(string _s) : n(_s.size() + 1), s(_s + "$"), sa_inv(s.begin(), s.end()), sa(n), lcp(n, 0){
+    vector<int> sa, sa_inv, lcp, tab_len;
+    vector<vector<int>> lcp_arr;
+    StringUtils(string _s) : n(_s.size() + 1), s(_s + "$"), sa_inv(s.begin(), s.end()), sa(n), lcp(n, 0), tab_len(n + 1, 0){
         vector<int> comp(sa_inv);
         sort(comp.begin(), comp.end());
         comp.erase(unique(comp.begin(), comp.end()), comp.end());
         for(int i = 0; i < n; ++i)
             sa_inv[i] = distance(comp.begin(), lower_bound(comp.begin(), comp.end(), sa_inv[i]));
-
         int m = comp.size();
         for(int i = 0; m != n; ++i){
             vector<vector<int>> table(m);
@@ -47,5 +47,19 @@ struct StringUtils{
             for(; i + h < n && j + h < n && s[i + h] == s[j + h]; ++h);
             lcp[sa_inv[i]] = h;
         }
+
+        lcp_arr.emplace_back(lcp);
+        for(int j = 0; (1 << j) < n; ++j){
+            lcp_arr.emplace_back(n);
+            for(int i = 0; i < n; ++i)
+                lcp_arr[j + 1][i] = (i + (1 << j) < n ? min(lcp_arr[j][i], lcp_arr[j][i + (1 << j)]) : lcp_arr[j][i]);
+        }
+        for(int i = 2; i <= n; ++i)
+            tab_len[i] = tab_len[i >> 1]+ 1;
+    }
+    int get_lcp(int l, int r){
+        int siz = r - l;
+        return min(lcp_arr[tab_len[siz]][l], lcp_arr[tab_len[siz]][r - (1 << tab_len[siz])]);
     }
 };
+
---
+++ verify/suffixarray_util.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/suffixarray"
+
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long;
+
+#include "../lib/functions/stringutils.cpp"
+
+
+signed main() {
+	string s;
+	cin >> s;
+	StringUtils st(s);
+	int n = s.size();
+	for(int i = 0; i < n; ++i)
+		cout << st.sa[i + 1] << " \n"[i == n - 1];
+}
